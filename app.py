6ab85c341adbf29ba964c1f2196cdbad3ef8cade
Rahul6158 <128017087+Rahul6158@users.noreply.github.com>
2025-01-05 14:33:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,80 +1,106 @@
-from dotenv import load_dotenv
-import os
-from PyPDF2 import PdfReader
 import streamlit as st
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain import FAISS
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import os
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import google.generativeai as genai
+from langchain.vectorstores import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
-from langchain.llms import OpenAI
-from langchain.callbacks import get_openai_callback
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
 
-import langchain
-langchain.verbose = False
-
-# load env varibales
 load_dotenv()
+os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 
-# process text from pdf
-def process_text(text):
-  # split the text into chunks using langchain
-  text_splitter = CharacterTextSplitter(
-    separator="\n",
-    chunk_size=1000,
-    chunk_overlap=200,
-    length_function=len
-  )
 
-  chunks = text_splitter.split_text(text)
 
-  # convert the chunks of text into embeddings to form a knowledge base
-  embeddings = OpenAIEmbeddings(openai_api_key=os.environ.get("OPENAI_API_KEY"))
 
-  knowledge_base = FAISS.from_texts(chunks, embeddings)
 
-  return knowledge_base
+def get_pdf_text(pdf_docs):
+    text=""
+    for pdf in pdf_docs:
+        pdf_reader= PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text+= page.extract_text()
+    return  text
+
+
+
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+
+def get_vector_store(text_chunks):
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+    vector_store.save_local("faiss_index")
+
+
+def get_conversational_chain():
+
+    prompt_template = """
+    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
+    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+    Context:\n {context}?\n
+    Question: \n{question}\n
+
+    Answer:
+    """
+
+    model = ChatGoogleGenerativeAI(model="gemini-pro",
+                             temperature=0.3)
+
+    prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+
+    return chain
+
+
+
+def user_input(user_question):
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+    
+    new_db = FAISS.load_local("faiss_index", embeddings)
+    docs = new_db.similarity_search(user_question)
+
+    chain = get_conversational_chain()
+
+    
+    response = chain(
+        {"input_documents":docs, "question": user_question}
+        , return_only_outputs=True)
+
+    print(response)
+    st.write("Reply: ", response["output_text"])
+
+
 
 
 def main():
-  st.title("Chat with my PDF")
+    st.set_page_config("Chat PDF")
+    st.header("Chat with PDF using Gemini💁")
 
-  pdf = st.file_uploader("Upload your PDF File", type="pdf")
+    user_question = st.text_input("Ask a Question from the PDF Files")
 
-  if pdf is not None:
-    pdf_reader = PdfReader(pdf)
-    
-    # store the pdf text in a var
-    text = ""
+    if user_question:
+        user_input(user_question)
 
-    for page in pdf_reader.pages:
-      text += page.extract_text()
+    with st.sidebar:
+        st.title("Menu:")
+        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
+        if st.button("Submit & Process"):
+            with st.spinner("Processing..."):
+                raw_text = get_pdf_text(pdf_docs)
+                text_chunks = get_text_chunks(raw_text)
+                get_vector_store(text_chunks)
+                st.success("Done")
 
-    # create a knowledge base object
-    knowledgeBase = process_text(text)
-
-    query = st.text_input('Ask question to PDF...')
-
-    cancel_button = st.button('Cancel')
-
-    if cancel_button:
-      st.stop()
-
-    if query:
-      docs = knowledgeBase.similarity_search(query)
-
-      llm = OpenAI(openai_api_key=os.environ.get("OPENAI_API_KEY"))
-
-      chain = load_qa_chain(llm, chain_type="stuff")
-
-
-      with get_openai_callback() as cost:
-        response = chain.invoke(input={"question": query, "input_documents": docs})
-        print(cost)
-
-        st.write(response["output_text"])
 
 
 if __name__ == "__main__":
-  main()
-
+    main()
